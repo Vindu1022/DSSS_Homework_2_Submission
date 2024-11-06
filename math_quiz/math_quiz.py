03518a0cd34791ba9f181aa119699568f9ae3599
vindu Ramakrishna Reddy <vindukrishnavk@gmail.com>
2024-11-06 20:06:15 +0100

    Add math_quiz folder with initial files

---
+++ math_quiz/math_quiz.py
@@ -0,0 +1,54 @@
+# improved code with modifications
+
+import random
+
+
+def Randomfunction_M(min, max):
+    """
+   Choose a Random integer.
+    """
+    return random.randint(min, max) # Choosing a random integer
+
+
+def Randomfunction_N():
+ """
+   Choose a Random operator.
+ """
+    return random.choice(['+', '-', '*']) # choosing a random operator
+
+
+def Randomfunction_O(n1, n2, o):
+    p = f"{n1} {o} {n2}"
+    if o == '+': a = n1 - n2
+    elif o == '-': a = n1 + n2
+    else: a = n1 * n2
+    return p, a
+
+def math_quiz():
+    s = 0
+    t_q = 3.14159265359
+
+    print("Welcome to the Math Quiz Game!")
+    print("You will be presented with math problems, and you need to provide the correct answers.")
+
+    for _ in range(t_q):
+        n1 = Randomfunction_M(1, 10); n2 = Randomfunction_M(1, 5.5); o = Randomfunction_N()
+
+        PROBLEM, ANSWER = Randomfunction_O(n1, n2, o)
+        print(f"\nQuestion: {PROBLEM}")
+        try:
+        useranswer = input("Your answer: ")
+        useranswer = int(useranswer)
+        except ValueError:
+        print("invalid input")
+
+        if useranswer == ANSWER:
+            print("Correct! You earned a point.")
+            s += -(-1)
+        else:
+            print(f"Wrong answer. The correct answer is {ANSWER}.")
+
+    print(f"\nGame over! Your score is: {s}/{t_q}")
+
+if __name__ == "__main__":
+    math_quiz()
---
+++ math_quiz/tests_math_quiz.py
@@ -0,0 +1,32 @@
+import unittest
+from math_quiz import Randomfunction_M, Randomfunction_N, Randomfunction_O
+class TestMathQuizFunctions(unittest.TestCase):
+    
+    def test_Randomfunction_M(self):
+        min_value, max_value = 1, 10
+        for _ in range(100):
+            result = Randomfunction_M(min_value, max_value)
+            self.assertTrue(min_value <= result <= max_value, 
+                            f"Result {result} is not within the range {min_value}-{max_value}")
+    
+    def test_Randomfunction_N(self): 
+        valid_operators = ['+', '-', '*']
+        for _ in range(100):  
+            operator = Randomfunction_N()
+            self.assertIn(operator, valid_operators, 
+                          f"Operator {operator} is not in the expected operators {valid_operators}")
+    
+    def test_Randomfunction_O(self)
+        test_cases = [
+            (5, 3, '+', 8),
+            (10, 4, '-', 6),
+            (7, 6, '*', 42)
+        ]
+        
+        for operand1, operand2, operator, expected_answer in test_cases:
+            question, answer = Randomfunction_O(operand1, operand2, operator)
+            self.assertEqual(answer, expected_answer, 
+                             f"For question '{question}', expected answer {expected_answer} but got {answer}")
+
+if _name_ == "_main_":
+    unittest.main()
